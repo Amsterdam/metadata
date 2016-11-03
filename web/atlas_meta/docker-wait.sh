ee04efacc092859fc72ab9a84f30301cb9405100
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-11-03 11:52:40 +0100

    Debug toolbar

--- web/atlas_meta/docker-wait.sh
+++ web/atlas_meta/docker-wait.sh
@@ -4,8 +4,9 @@ set -u
 set -e
 
 # wait for postgres
+echo "Waiting for postgres."
 while ! nc -z ${DATABASE_PORT_5432_TCP_ADDR} ${DATABASE_PORT_5432_TCP_PORT}
 do
-	echo "Waiting for postgres..."
+	echo "Still waiting for postgres..."
 	sleep 0.1
 done
