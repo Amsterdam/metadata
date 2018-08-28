428fc66911cee475acc2ea0a3efab26a6d011995
Bart Heupers <bart.heupers@gmail.com>
2018-08-28 15:00:15 +0200

    Overlastgebieden toegevoegd

---
+++ web/atlas_meta/atlas/migrations/0007_ovv_gebieden.py
@@ -0,0 +1,42 @@
+from django.db import migrations, IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
+
+NEW_DATA = (
+    {
+        'id': 'gebieden-ovv',
+        'title': 'Overlastgebieden',
+        'group': 'Gebieden',
+        'update_frequency': 'op verzoek',
+        'data_modified_date': 'augustus 2018',
+    },
+)
+
+
+def update_ovv_gebieden(apps, schema_editor):
+    Migrations = apps.get_model('atlas', 'metadata')
+
+    # Delete gebieden_ovv, if it exists
+    try:
+        gebieden_ovv = Migrations.objects.get(pk='gebieden-ovv')
+    except ObjectDoesNotExist:
+        pass  # this row doesn't exist
+    else:
+        gebieden_ovv.delete()
+
+    # insert the splitted rows, if they don't exist
+    for row in NEW_DATA:
+        try:
+            Migrations.objects.create(**row)
+        except IntegrityError:
+            pass  # a row with this PK already exists.
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('atlas', '0006_nap_meetbouten'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_ovv_gebieden),
+    ]
