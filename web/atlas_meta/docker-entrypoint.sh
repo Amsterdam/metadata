5cf5656132d8bb9951392593d8f9eaf7291352e9
Stephan Zaat <s.zaat@amsterdam.nl>
2019-03-20 13:16:21 +0100

    Update static files

--- web/atlas_meta/docker-entrypoint.sh
+++ web/atlas_meta/docker-entrypoint.sh
@@ -8,5 +8,12 @@ cd /app
 # migrate database tables
 yes yes | python manage.py migrate --noinput
 
+echo Collecting static files
+yes yes | python manage.py collectstatic
+
+ls -al /static/
+
+chmod -R 777 /static
+
 # run uwsgi
 exec uwsgi
